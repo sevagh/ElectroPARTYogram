cad1d9eb0d8dd24b5f1159ce0e17a96dd1c5f67c
Sevag Hanssian <sevag.hanssian@gmail.com>
2020-02-05 11:25:58 -0500

    Interesting visuals for now

--- app/src/main/cpp/GraphicsLoop.cpp
+++ app/src/main/cpp/GraphicsLoop.cpp
@@ -5,11 +5,16 @@ static bool abs_compare_f(float a, float b)
     return (std::fabs(a) < std::fabs(b));
 }
 
+void graphics::GraphicsLoop::recomputePeriod() {
+    period = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::duration<float, std::ratio<1, 1000000>>(tempo/(60.0F*1000000.0F)));
+}
+
 void graphics::GraphicsLoop::createBeatTrackArt(const DrawParams *draw) {
     auto viewSize = view.getSize();
 
     // copy FFT data
-    std::vector<float> fftArray(
+    std::vector<float> fftMag(
             audioEngine.recordingCallback.beatDetector.odf.magSpecCopy,
             audioEngine.recordingCallback.beatDetector.odf.magSpecCopy + btrack::OnsetDetectionFunction::FrameSize);
 
@@ -24,40 +29,39 @@ void graphics::GraphicsLoop::createBeatTrackArt(const DrawParams *draw) {
     // draw FFT from left to right on bottom
     auto audioMax = std::max_element(audioArray.begin(), audioArray.end(), abs_compare_f);
     audioOverallMax = std::max(*audioMax, audioOverallMax);
-    auto fftMax = std::max_element(fftArray.begin(), fftArray.end(), abs_compare_f);
-    fftOverallMax = std::max(*fftMax, fftOverallMax);
+    auto fftMax = std::max_element(fftMag.begin(), fftMag.end(), abs_compare_f);
+    fftMagOverallMax = std::max(*fftMax, fftMagOverallMax);
     sf::VertexArray audioCurve(sf::PrimitiveType::LinesStrip, btrack::BTrack::FrameSize);
     sf::VertexArray fftCurve(sf::PrimitiveType::LinesStrip, btrack::BTrack::FrameSize);
     size_t fftIndex = 0;
     for (size_t i = 0; i < btrack::BTrack::FrameSize; ++i) {
         audioCurve[i] = sf::Vertex(sf::Vector2f(pos, (audioArray[i]/audioOverallMax)*0.25F*viewSize.y));
-        fftCurve[i] = sf::Vertex(sf::Vector2f(pos, viewSize.y-(fftArray[fftIndex]/fftOverallMax)*0.25F*viewSize.y));
+        fftCurve[i] = sf::Vertex(sf::Vector2f(pos, viewSize.y-(fftMag[fftIndex]/fftMagOverallMax)*0.25F*viewSize.y));
         pos += hopSize;
         // fill every other element for FFT since only half is valuable
         if (i % 2) {
             fftIndex++;
         }
     }
+
     mainWindow.draw(audioCurve);
     mainWindow.draw(fftCurve);
+
+    cumScoreOverallMax = std::max(cumScoreOverallMax, draw->cumScore);
+    currentCumScore = draw->cumScore/cumScoreOverallMax; // at most 1.0 for high energy portions
+    sf::Color color((uint8_t)(255*currentCumScore), 0, (uint8_t)(255*(1.0-currentCumScore)), (uint8_t)(255*currentCumScore));
 
     // make a beat circle
     sf::CircleShape circle(viewSize.x/4.0F);
 
+    if (draw->beat) {
+        circle.setRadius(circle.getRadius()*1.25F);
+    }
+
     // center the circle
     circle.setOrigin(circle.getRadius(), circle.getRadius());
-    circle.setFillColor(sf::Color::Black);
     circle.setPosition(viewSize.x/2.0F, viewSize.y/2.0F);
-    circle.setOutlineColor(sf::Color::Green);
-    circle.setOutlineThickness(5.0F);
-
-    if (draw->beat) {
-        timer = FramesVisible;
-        circle.setFillColor(sf::Color::Red); // on a beat, fill the circle red
-    } else if (timer > 0) { // keep drawing the last circle
-        circle.setFillColor(sf::Color::Red); // on a beat, fill the circle red
-        timer--;
-    }
+    circle.setFillColor(color);
     mainWindow.draw(circle);
 }
 
--- app/src/main/cpp/GraphicsLoop.h
+++ app/src/main/cpp/GraphicsLoop.h
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include "AudioEngine.h"
 #include <optional>
+#include <chrono>
 
 #ifndef ELECTROPARTYOGRAM_GRAPHICSLOOP_H
 #define ELECTROPARTYOGRAM_GRAPHICSLOOP_H
@@ -18,8 +19,14 @@ namespace graphics {
            , touch(true)
            , timer(0)
            , view(mainWindow.getDefaultView())
-           , fftOverallMax(-FLT_MAX)
+           , fftMagOverallMax(-FLT_MAX)
            , audioOverallMax(-FLT_MAX)
+           , cumScoreOverallMax(-FLT_MAX)
+           , currentCumScore(0.0F)
+           , tempo(100) // start at 100bpm
+           , period(std::chrono::duration_cast<std::chrono::microseconds>(
+                       std::chrono::duration<float, std::ratio<1, 1000000>>(tempo/(60.0F*1000000.0F))))
+           , drawOuterCircle(false)
        {
            mainWindow.setVerticalSyncEnabled(true);
        };
@@ -27,21 +34,28 @@ namespace graphics {
        void loop();
 
    private:
-       static constexpr int FramesVisible = 32;
+       static constexpr int FramesVisible = 128;
        sf::RenderWindow mainWindow;
        AudioEngine audioEngine;
        bool focus;
        bool touch;
        float touch_pos_1;
        float touch_pos_2;
-       float fftOverallMax;
+       float fftMagOverallMax;
        float audioOverallMax;
+       float cumScoreOverallMax;
+       float currentCumScore;
+       float tempo;
+       std::chrono::microseconds period;
+       std::chrono::steady_clock::time_point lastBeat;
        int timer;
        sf::View view;
        sf::Event event;
+       bool drawOuterCircle;
 
        void createBeatTrackArt(const DrawParams *draw);
        void createFingerArt(const float x, const float y);
+       void recomputePeriod();
    };
 } // namespace graphics
 
